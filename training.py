cf767716309f11353553c8cb7054bad485f7b41e
Arpan Mukherjee <arpn@Arpans-Air.Dlink>
2020-01-02 23:41:01 +0530

    Fixed some bugs in MNIST data

--- training.py
+++ training.py
@@ -11,24 +11,18 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader, Dataset
 
 def main(args = None):
-	T = transforms.Compose(
-	[transforms.Grayscale(num_output_channels = 1),
-	transforms.ToTensor(),
-	transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # transformation
-
-	ip_dim = 96*96 # input dimension
-	h1 = int(ip_dim/2) # hidden layer 1 dimension
-	op_dim = int(ip_dim/4) # output dimension
-
 	parser = argparse.ArgumentParser(description='Simple training script for training a Vanilla Autoencoder network.')
 
-	parser.add_argument('--data_path', help = 'Path for the downloaded dataset', default = './dataset/')
+	parser.add_argument('--data-path', help = 'Path for the downloaded dataset', default = '../dataset/')
 	parser.add_argument('--dataset', help = 'Dataset name. Must be one of MNIST, STL10, CIFAR10')
 	parser.add_argument('--epochs', help = 'Number of epochs', type = int, default = 75)
-	parser.add_argument('--batch_size', help = 'Batch size of the data', type = int, default = 28)
-	parser.add_argument('--learning_rate', help = 'Learning rate', type = float, default = 0.001)
-	parser.add_argument('--use_cuda', help = 'CUDA usage', type = bool)
-	parser.add_argument('--network_type', help='Type of the network layers. Must be one of Conv, FC', default='FC')
+	parser.add_argument('--batch-size', help = 'Batch size of the data', type = int, default = 16)
+	parser.add_argument('--learning-rate', help = 'Learning rate', type = float, default = 0.001)
+	parser.add_argument('--use-cuda', help = 'CUDA usage', type = bool, default = False)
+	parser.add_argument('--network-type', help = 'Type of the network layers. Must be one of Conv, FC', default = 'FC')
+	parser.add_argument('--seed', help = 'Random seed (default:1)', type = int, default = 1)
+	parser.add_argument('--log-interval', help = 'how many batches to wait before logging training status', type = int, default = 100)
+	parser.add_argument('--save-model', help = 'For saving the current model', type = bool, default = True)
 
 	parser = parser.parse_args(args)
 
@@ -43,18 +37,45 @@ def main(args = None):
 	else:
 		data_path = parser.data_path
 		if not os.path.isdir(data_path):
-    		os.mkdir(data_path)
+			os.mkdir(data_path)
 
 	# Downloading proper dataset and creating data loader
 	if parser.dataset == 'MNIST':
-		train_data = torchvision.datasets.MNIST(data_path, train=True, download=True, transforms=T)
-		test_data = torchvision.datasets.MNIST(data_path, train=False, download=True, transforms=T)
+        T = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+		train_data = torchvision.datasets.MNIST(data_path, train = True, download = True, transform = T)
+        test_data = torchvision.datasets.MNIST(data_path, train = False, download = True, transform = T)
+
+		ip_dim = 1*28*28 # input dimension
+		h1_dim = int(ip_dim/2) # hidden layer 1 dimension
+		op_dim = int(ip_dim/4) # output dimension
 	elif parser.dataset == 'STL10':
-		train_data = torchvision.datasets.STL10(data_path, split='train', download=True, transforms=T)
-		test_data = torchvision.datasets.STL10(data_path, split='test', download=True, transforms=T)
+        T = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+		train_data = torchvision.datasets.STL10(data_path, train = True, download = True, transform = T)
+        test_data = torchvision.datasets.STL10(data_path, train = False, download = True, transform = T)
+
+		ip_dim = 3*96*96 # input dimension
+		h1_dim = int(ip_dim/2) # hidden layer 1 dimension
+		op_dim = int(ip_dim/4) # output dimension
 	elif parser.dataset == 'CIFAR10':
-		train_data = torchvision.datasets.CIFAR10(data_path, train=True, download=True, transforms=T)
-		test_data = torchvision.datasets.CIFAR10(data_path, train=False, download=True, transforms=T)
+        T = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+		train_data = torchvision.datasets.CIFAR10(data_path, train = True, download = True, transform = T)
+        test_data = torchvision.datasets.CIFAR10(data_path, train = False, download = True, transform = T)
+
+		ip_dim = 3*32*32 # input dimension
+		h1_dim = int(ip_dim/2) # hidden layer 1 dimension
+		op_dim = int(ip_dim/4) # output dimension
 	elif parser.dataset is None:
 		raise ValueError('Must provide dataset name')
 	else:
@@ -80,25 +101,27 @@ def main(args = None):
 
 	# Type of layer
 	if parser.network_type == 'FC':
-		auto_encoder = model.Autoencoder(ip_dim, h_dim, op_dim).to(device)
+		auto_encoder = Autoencoder(ip_dim, h1_dim, op_dim).to(device)
 	elif parser.network_type == 'Conv':
-		auto_encoder = model.ConvolutionAE().to(device)
+		auto_encoder = ConvolutionAE().to(device)
 	else:
 		raise ValueError('Network type must be either FC or Conv type')
 
 	# Show some real images
-	data_iter = iter(train_loader)
-	images, labels = data_iter.next()
+	for images, labels in train_loader:
+		break
+	# data_iter = iter(train_loader)
+	# images, labels = data_iter.next()
 	imshow(torchvision.utils.make_grid(images))
 
 	# Train the model
 	auto_encoder.train()
 	criterion = nn.MSELoss()
-	optimizer = torch.optim.Adam(lr=learning_rate, params=auto_encoder.parameters(), weight_decay=1e-5)
+	optimizer = torch.optim.Adam(lr = learning_rate, params = auto_encoder.parameters(), weight_decay = 1e-5)
 
 	for n_epoch in range(epochs): # loop over the dataset multiple times
 		reconstruction_loss = 0.0
-		for i, X, Y in enumerate(train_loader, 0):
+		for i, (X, Y) in enumerate(train_loader):
 			X = X.view(X.size()[0], -1)
 			X = Variable(X).to(device)
 			Y = Variable(Y).to(device)
--- visualize.py
+++ visualize.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def imshow(img):
-    img = img / 2 + 0.5 # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+	img = img / 2 + 0.5 # unnormalize
+	npimg = img.numpy()
+	print(npimg.shape)
+	plt.imshow(np.transpose(npimg, (1, 2, 0)))
+	plt.show()
