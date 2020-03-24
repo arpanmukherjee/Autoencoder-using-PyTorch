ed376433d3622025847c6c0fe005723fcce43b42
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-03-24 20:54:03 +0530

    Added comments

--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def imshow(img):
+    """This function shows the image using
+
+    Arguments:
+        img {np array} -- Image numpy array
+    """
     img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     print(npimg.shape)
